7acfa983d374b22006c1b4db15265a79549e0a84
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-07-29 12:02:01 +0100

    replacing fromvalue with postformvalue, fixing the html

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,25 +1,24 @@
 package funcs
 
 import (
+	"fmt"
 	"html/template"
+	fs "my-ascii-art-web/fs"
 	"net/http"
 	"os"
-	"fmt"
-	fs "my-ascii-art-web/fs"
 	"strings"
 )
+
 var t *template.Template
 
 var err error
 
 func init() {
-	t, err = template.ParseFiles("templates/index.html") 
+	t, err = template.ParseFiles("templates/index.html")
 	if err != nil {
 		panic(err)
 	}
 }
-
-
 
 var (
 	Index_path        = "templates/index.html"
@@ -87,9 +86,9 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func Ascii_Art(w http.ResponseWriter, r *http.Request) {
 	Data.Shown = false
-	// this is only for the testing function it is not needed 
+	// this is only for the testing function it is not needed
 	// for the the server to operate normally.
-	if r.URL.Path != "/ascii-art"{
+	if r.URL.Path != "/ascii-art" {
 		ErrorFunc(w, http.StatusNotFound)
 	}
 	if r.Method != http.MethodPost {
@@ -108,9 +107,18 @@ func Ascii_Art(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// parse the form into a map and get the needed value
-	r.ParseForm()
-	Input.Text = r.FormValue("text")
-	Input.Banner = r.FormValue("banner")
+	// Parse the form data
+	err := r.ParseForm()
+	if err != nil {
+		// Handle error
+		ErrorFunc(w, 404)
+	}
+	// Get the needed values from the form
+	
+	Input.Text = r.PostFormValue("text")
+	Input.Banner = r.PostFormValue("banner")
+	//Input.Text = r.FormValue("text")
+	//Input.Banner = r.FormValue("banner")
 	Data.Ascii, Input.Status, Input.Err = fs.Ascii_Art(Input.Text, Input.Banner)
 	if Input.Err != nil && Input.Status != 200 {
 		ErrorFunc(w, Input.Status)
@@ -120,13 +128,13 @@ func Ascii_Art(w http.ResponseWriter, r *http.Request) {
 	Data.Shown = true
 }
 
-func Download(w http.ResponseWriter, r *http.Request){
+func Download(w http.ResponseWriter, r *http.Request) {
 	// i have to empty the data after use
-	if r.URL.Path != "/download"{
-		ErrorFunc(w,http.StatusNotFound) 
+	if r.URL.Path != "/download" {
+		ErrorFunc(w, http.StatusNotFound)
 		return
 	}
-	if r.Method != http.MethodGet{
+	if r.Method != http.MethodGet {
 		ErrorFunc(w, http.StatusMethodNotAllowed)
 		return
 	}
@@ -134,31 +142,32 @@ func Download(w http.ResponseWriter, r *http.Request){
 	if referer != "http://localhost:8080/" {
 		fmt.Println("found error at refere")
 		ErrorFunc(w, http.StatusForbidden)
-        	return
+		return
 	}
-	// creat the file first 
+	// creat the file first
 	filename := "ascii-art.txt"
 	err := os.WriteFile(filename, []byte(ToDownload), 0644)
 	if err != nil {
 		ErrorFunc(w, http.StatusInternalServerError)
 		return
 	}
-	
+
 	w.Header().Set("Content-Disposition", "attachment; filename=ascii-art.txt")
-     	w.Header().Set("Content-Type", "text/plain")
-	http.ServeFile(w, r ,filename)
+	w.Header().Set("Content-Type", "text/plain")
+	http.ServeFile(w, r, filename)
 	os.Remove(filename)
 
 }
+
 // using a middleware
 func MiddleWare(next http.Handler) http.Handler {
 	// the case of examle /something/
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if strings.HasSuffix(r.URL.Path, "/") {
-            ErrorFunc(w,404 )
-            return
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			ErrorFunc(w, 404)
+			return
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
